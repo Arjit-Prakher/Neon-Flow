c817f4a4790c85efa58f19624abec0aef06b9514
Arjit Prakher <arjitprakher4@gmail.com>
2026-06-23 22:12:04 +0530

    Updated CORS

--- server/server.js
+++ server/server.js
@@ -23,7 +23,8 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin: "https://neon-flow-phi.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
